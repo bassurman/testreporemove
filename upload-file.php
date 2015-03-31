d15aab23546f1afffdaad00d130c8bb3ee85e715
grigorenko <a.grigorenko.belvg@gmail.com>
2015-03-31 17:47:09 +0300

    test changes

--- upload-file.php
+++ upload-file.php
@@ -1,6 +1,14 @@
 <?php
 $uploaddir = './files/';
+$uploaddir = './files/';
+$uploaddir = './files/';
+$uploaddir = './files/';
+
 $file = $uploaddir . basename($_FILES['uploadfile']['name']); 
 if (move_uploaded_file($_FILES['uploadfile']['tmp_name'], $file)) {echo "success";}
  else {echo "error";}
+
+$uploaddir = './files/';
+$uploaddir = './files/';
+$uploaddir = './files/';
 ?>
